2fdd57e9810ff3950ac52216a9b7127ea64eb26c
Ravinder Khangura <38717332+ravinderkhangura@users.noreply.github.com>
2018-10-30 10:39:10 +0530

    pointer

--- exercises/binary-search/src/example.c
+++ exercises/binary-search/src/example.c
@@ -1,11 +1,11 @@
-#include "binary_search.h"
+#include "binary_search.h" 
 
 int *binary_search(const int value, const int *arr, const size_t length)
 {
    if (0 == length || NULL == arr) {
       return NULL;
    }
-   const int *low = arr;
+   const int *low = arr;//pointer
    const int *high = arr + length - 1;
    while (low <= high) {
       const int *mid = low + (high - low) / 2;
